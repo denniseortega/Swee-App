d557c3b05ff6bfc9263f7df757bb9e2e7c114335
Dennise Ortega <dennise.i.ortega@gmail.com>
2020-03-02 19:26:55 -0800

    Implement GridView for video

--- lib/screens/video/video.dart
+++ lib/screens/video/video.dart
@@ -1,25 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-
-List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
-  const StaggeredTile.count(2, 2),
-  const StaggeredTile.count(2, 1),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(2, 2),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(3, 1),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(4, 1),
-];
-
-List<Widget> _tiles = const <Widget>[
-
-];
 
 class VideoPlayerScreen extends StatefulWidget {
   VideoPlayerScreen({Key key}) : super(key: key);
@@ -29,27 +10,28 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  VideoPlayerController _controller;
-  Future<void> _initializeVideoPlayerFuture;
+  List<VideoPlayerController> _controllers = [];
+  List<Future<void>> _controllersInit = [];
+  List<String> videos = ["https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4", "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4", "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4", "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4", "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4"];
 
   @override
   void initState() {
-    _controller = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
-    );
+    for (var i = 0; i < videos.length; i++) {
+      _controllers.add(VideoPlayerController.network(videos[i]));
+      _controllersInit.add(_controllers[i].initialize());
+      _controllers[i].setLooping(true);
+    } 
 
-    // Initialize the controller and store the Future for later use.
-    _initializeVideoPlayerFuture = _controller.initialize();
-
-    // Use the controller to loop the video.
-    _controller.setLooping(true);
     super.initState();
   }
 
   @override
   void dispose() {
     // Ensure disposing of the VideoPlayerController to free up resources.
-    _controller.dispose();
+    // _controller.dispose();
+    for (var i = 0; i < _controllers.length; i++) {
+      _controllers[i].dispose();
+    } 
 
     super.dispose();
   }
@@ -57,74 +39,67 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: Drawer(),
-      backgroundColor: Colors.purpleAccent,
+      backgroundColor: Colors.white,
         appBar: AppBar(title: Text('Video')),
-      // appBar: AppBar(
-      //   title: Text('Golf Video'),
-      //   backgroundColor: Colors.black87,
-      // ),
-      // Use a FutureBuilder to display a loading spinner while waiting for the
-      // VideoPlayerController to finish initializing.
-      body: Stack(
-        children: <Widget>[
-          Center(child:FutureBuilder(
-            future: _initializeVideoPlayerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                // If the VideoPlayerController has finished initialization, use
-                // the data it provides to limit the aspect ratio of the video.
-                return AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  // Use the VideoPlayer widget to display the video.
-                  child: VideoPlayer(_controller),
-                );
-              } else {
-                // If the VideoPlayerController is still initializing, show a
-                // loading spinner.
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-          )),
-          Center(
-              child:
-             ButtonTheme(
-                  height: 100.0,
-                  minWidth: 200.0,
-                  child: RaisedButton(
-                    padding: EdgeInsets.all(60.0),
-                    color: Colors.transparent,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      // Wrap the play or pause in a call to `setState`. This ensures the
-                      // correct icon is shown.
-                      setState(() {
-                        // If the video is playing, pause it.
-                        if (_controller.value.isPlaying) {
-                          _controller.pause();
+      body: 
+      GridView.builder(
+            itemCount: _controllers.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+            itemBuilder: (BuildContext context, int index){
+              // return VideoPlayer(_controller);
+              return Stack(
+                  children: <Widget>[
+                    Center(child:FutureBuilder(
+                      // future: _initializeVideoPlayerFuture,
+                      future: _controllersInit[index],
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          // If the VideoPlayerController has finished initialization, use
+                          // the data it provides to limit the aspect ratio of the video.
+                          return AspectRatio(
+                            aspectRatio: _controllers[index].value.aspectRatio,
+                            // Use the VideoPlayer widget to display the video.
+                            child: VideoPlayer(_controllers[index]),
+                          );
                         } else {
-                          // If the video is paused, play it.
-                          _controller.play();
+                          // If the VideoPlayerController is still initializing, show a
+                          // loading spinner.
+                          return Center(child: CircularProgressIndicator());
                         }
-                      });
-                    },
-                    child: Icon(
-                      _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-                      size: 120.0,
-                    
-                      child: new StaggeredGridView.count(
-                        crossAxisCount: 4,
-                        staggeredTiles: _staggeredTiles,
-                        children: _tiles,
-                        mainAxisSpacing: 4.0,
-                        crossAxisSpacing: 4.0,
-                        padding: const EdgeInsets.all(4.0),
-                      ),
-                    ),
-                  ))
-          )
-        ],
-      ),
+                      },
+                    )),
+                    Center(
+                        child:
+                      ButtonTheme(
+                            height: 100.0,
+                            minWidth: 200.0,
+                            child: RaisedButton(
+                              padding: EdgeInsets.all(60.0),
+                              color: Colors.transparent,
+                              textColor: Colors.white,
+                              onPressed: () {
+                                // Wrap the play or pause in a call to `setState`. This ensures the
+                                // correct icon is shown.
+                                setState(() {
+                                  // If the video is playing, pause it.
+                                  if (_controllers[index].value.isPlaying) {
+                                    _controllers[index].pause();
+                                  } else {
+                                    // If the video is paused, play it.
+                                    _controllers[index].play();
+                                  }
+                                });
+                              },
+                              child: Icon(
+                                _controllers[index].value.isPlaying ? Icons.pause : Icons.play_arrow,
+                                size: 120.0,
+                              ),
+                            ))
+                    )
+                  ],
+                );
+            },
+        ),
     );
   }
 }
