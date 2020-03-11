d79903b6f4e166c7614d13c0fee6a33125f46e71
Alex <Alex@SweeMBP.local>
2020-03-10 19:20:11 -0700

    Add ios files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/DenniseOrtega/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/DenniseOrtega/code/flutter_projects/swee_app2"
-export "FLUTTER_TARGET=/Users/DenniseOrtega/code/flutter_projects/swee_app2/lib/main.dart"
+export "FLUTTER_ROOT=/Users/Alex/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Alex/development/swee-app"
+export "FLUTTER_TARGET=/Users/Alex/development/swee-app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/DenniseOrtega/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Alex/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,13 +3,54 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<camera/CameraPlugin.h>)
 #import <camera/CameraPlugin.h>
+#else
+@import camera;
+#endif
+
+#if __has_include(<connectivity/ConnectivityPlugin.h>)
 #import <connectivity/ConnectivityPlugin.h>
+#else
+@import connectivity;
+#endif
+
+#if __has_include(<flutter_downloader/FlutterDownloaderPlugin.h>)
 #import <flutter_downloader/FlutterDownloaderPlugin.h>
+#else
+@import flutter_downloader;
+#endif
+
+#if __has_include(<get_ip/GetIpPlugin.h>)
 #import <get_ip/GetIpPlugin.h>
+#else
+@import get_ip;
+#endif
+
+#if __has_include(<image_picker/ImagePickerPlugin.h>)
 #import <image_picker/ImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
 #import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+#if __has_include(<video_player/VideoPlayerPlugin.h>)
 #import <video_player/VideoPlayerPlugin.h>
+#else
+@import video_player;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
@@ -20,6 +61,7 @@
   [GetIpPlugin registerWithRegistrar:[registry registrarForPlugin:@"GetIpPlugin"]];
   [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
   [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
 }
 
